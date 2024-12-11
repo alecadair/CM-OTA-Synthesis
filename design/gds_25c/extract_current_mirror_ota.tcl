e7ed66ad79bc75e71333d2af6417ce3eb48dbb9e
alecadair <alecadair1@gmail.com>
2024-12-11 21:03:05 +0200

    fixed filepaths for tcl scripts

--- design/gds_25c/extract_current_mirror_ota.tcl
+++ design/gds_25c/extract_current_mirror_ota.tcl
@@ -1,7 +1,7 @@
 set pdk_root $env(PDK_ROOT)
 
-source eda/sky130.magicrc
-gds read design/gds_25c/cm_ota_align/CURRENT_MIRROR_OTA_0.gds
+source CM-OTA-Synthesis/eda/sky130.magicrc
+gds read CM-OTA-Synthesis/design/gds_25c/cm_ota_align/CURRENT_MIRROR_OTA_0.gds
 load CURRENT_MIRROR_OTA_0
 extract unique
 extract all
